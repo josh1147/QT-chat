6f5dc4e855d76fb8d965d3cdaf4d13445a6e793a
josh1147 <2300012902@stu.pku.edu.cn>
2024-06-30 22:21:34 +0800

    completed2

--- client/login.cpp
+++ client/login.cpp
@@ -3,44 +3,44 @@
 #include "dialog.h"
 #include <QMessageBox>
 #include <QSqlQuery>
-login::login(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::login)
+login::login(QWidget *parent) : QDialog(parent),
+                                ui(new Ui::login)
 {
     ui->setupUi(this);
 
-
     // this->setStyleSheet("background-color: red;");
-    ui->pwdline->setStyleSheet("font: 25 14pt '微软雅黑 Light';" //字体
-                                "color: rgb(31,31,31);"		//字体颜色
-                                "padding-left:20px;"       //内边距-字体缩进
-                                "background-color: rgb(255, 255, 255);" //背景颜色
-                                "border:2px solid rgb(20,196,188);border-radius:15px;");//边框粗细-颜色-圆角设置
-    ui->userline->setStyleSheet("font: 25 14pt '微软雅黑 Light';" //字体
-                               "color: rgb(31,31,31);"		//字体颜色
-                               "padding-left:20px;"       //内边距-字体缩进
-                               "background-color: rgb(255, 255, 255);" //背景颜色
-                               "border:2px solid rgb(20,196,188);border-radius:15px;}");//边框粗细-颜色-圆角设置
+    ui->pwdline->setStyleSheet("font: 25 14pt '微软雅黑 Light';"                          // 字体
+                               "color: rgb(31,31,31);"                                    // 字体颜色
+                               "padding-left:20px;"                                       // 内边距-字体缩进
+                               "background-color: rgb(255, 255, 255);"                    // 背景颜色
+                               "border:2px solid rgb(20,196,188);border-radius:15px;");   // 边框粗细-颜色-圆角设置
+    ui->userline->setStyleSheet("font: 25 14pt '微软雅黑 Light';"                         // 字体
+                                "color: rgb(31,31,31);"                                   // 字体颜色
+                                "padding-left:20px;"                                      // 内边距-字体缩进
+                                "background-color: rgb(255, 255, 255);"                   // 背景颜色
+                                "border:2px solid rgb(20,196,188);border-radius:15px;}"); // 边框粗细-颜色-圆角设置
 
-    ui->loginbtn->setStyleSheet("color : yellow;" "width : 50px;") ;
+    ui->loginbtn->setStyleSheet("color : yellow;"
+                                "width : 50px;");
     this->setStyleSheet(
         "background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(251,102,102, 200),"
         " stop:1 rgba(20,196,188, 210));");
     ui->registerbtn->setStyleSheet("QPushButton{font: 25 14pt '微软雅黑 Light';color: yellow;"
-                                "background-color: rgb(20,196,188);"
-                                "border: none;border-radius:15px;}"
-                                "QPushButton:hover{background-color: rgb(22,218,208);}"//hover
-                                "QPushButton:pressed{background-color: rgb(17,171,164);}");//pressed
-    ui->loginbtn->setStyleSheet("QPushButton{font: 25 14pt '微软雅黑 Light';color: yellow;"
                                    "background-color: rgb(20,196,188);"
                                    "border: none;border-radius:15px;}"
-                                   "QPushButton:hover{background-color: rgb(22,218,208);}"//hover
-                                   "QPushButton:pressed{background-color: rgb(17,171,164);}");//pressed
-    ui->label->setStyleSheet("font: 25 15pt '微软雅黑 Light';" "background-color:rgba(255,255,255,0)");
-    ui->label_2->setStyleSheet("font: 25 12pt '微软雅黑 Light';" "background-color:rgba(255,255,255,0)");
-    ui->label_3->setStyleSheet("font: 25 12pt '微软雅黑 Light';" "background-color:rgba(255,255,255,0)");
-
-
+                                   "QPushButton:hover{background-color: rgb(22,218,208);}"     // hover
+                                   "QPushButton:pressed{background-color: rgb(17,171,164);}"); // pressed
+    ui->loginbtn->setStyleSheet("QPushButton{font: 25 14pt '微软雅黑 Light';color: yellow;"
+                                "background-color: rgb(20,196,188);"
+                                "border: none;border-radius:15px;}"
+                                "QPushButton:hover{background-color: rgb(22,218,208);}"     // hover
+                                "QPushButton:pressed{background-color: rgb(17,171,164);}"); // pressed
+    ui->label->setStyleSheet("font: 25 15pt '微软雅黑 Light';"
+                             "background-color:rgba(255,255,255,0)");
+    ui->label_2->setStyleSheet("font: 25 12pt '微软雅黑 Light';"
+                               "background-color:rgba(255,255,255,0)");
+    ui->label_3->setStyleSheet("font: 25 12pt '微软雅黑 Light';"
+                               "background-color:rgba(255,255,255,0)");
 }
 
 login::~login()
@@ -50,25 +50,30 @@ login::~login()
 
 void login::on_loginbtn_clicked()
 {
-    QString user ;
-    QString pwd  ;
+    QString user;
+    QString pwd;
     user = ui->userline->text();
-    pwd  = ui->pwdline->text();
-    if(user=="")
-        QMessageBox::warning(this,"","用户名不能为空");
-    else if(pwd=="")
-            QMessageBox::warning(this,"","密码不能为空");
-    else{
-        //QString S = QString(“select * from user_table where name=’%1’ and password=’%2’”).arg(user).arg(pwd);
+    pwd = ui->pwdline->text();
+    if (user == "")
+        QMessageBox::warning(this, "", "用户名不能为空");
+    else if (pwd == "")
+        QMessageBox::warning(this, "", "密码不能为空");
+    else
+    {
+        // QString S = QString(“select * from user_table where name=’%1’ and password=’%2’”).arg(user).arg(pwd);
         QString S = QString("SELECT * FROM user_table WHERE name='%1' AND password='%2'").arg(user).arg(pwd);
-        QSqlQuery query ;
-        if(query.exec(S)){
-            QMessageBox::information(NULL, "登陆成功", "登陆成功！！！", QMessageBox::Yes);
-            present_id = user ;
-            accept();
+        QSqlQuery query;
+        if (query.exec(S))
+        {
+            if (query.next())
+            {
+                QMessageBox::information(NULL, "登陆成功", "登陆成功！！！", QMessageBox::Yes);
+                present_id = user;
+                accept();
+            }
+            else
+                QMessageBox::warning(this, "error", "用户名或者密码错误！！");
         }
-        else
-            QMessageBox::warning(this,"error","用户名或者密码错误！！");
 
         ui->userline->clear();
         ui->pwdline->clear();
@@ -78,6 +83,6 @@ void login::on_loginbtn_clicked()
 
 void login::on_registerbtn_clicked()
 {
-    Dialog dlg ;
+    Dialog dlg;
     dlg.exec();
 }
--- client/widget.cpp
+++ client/widget.cpp
@@ -1,40 +1,41 @@
 #include "widget.h"
 #include "ui_widget.h"
 
-Widget::Widget(const QString &name,const int &port,QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Widget) , id(name)
+Widget::Widget(const QString &name, const int &port, QWidget *parent)
+    : QWidget(parent), ui(new Ui::Widget), id(name)
 {
     ui->setupUi(this);
 
-    setWindowFlags(windowFlags()& ~Qt::WindowMaximizeButtonHint);
+    setWindowFlags(windowFlags() & ~Qt::WindowMaximizeButtonHint);
     setFixedSize(this->width(), this->height());
     socket = new QTcpSocket(this);
     ui->your_id->setText(name);
-    ui->room_label->setText(QString("%1号大厅").arg(port/100));
-    openDatabase();//
-    createTable();//
+    ui->room_label->setText(QString("%1号大厅").arg(port / 100));
+    openDatabase();     //
+    createTable();      //
     clearChatHistory(); // 清空聊天记录
     QString ip = QString("127.0.0.1");
     socket->abort();
-    socket->connectToHost(ip,port);
+    socket->connectToHost(ip, port);
     bool isconnect = socket->waitForConnected();
-    if(isconnect){
+    if (isconnect)
+    {
         ui->textEdit->append("The connection wal successful!");
         ui->btn_push->setEnabled(true);
 
         QByteArray tmp = QString("#%1").arg(id).toUtf8();
         socket->write(tmp);
-        connect(socket,&QTcpSocket::readyRead,this,&Widget::ReadData);
-
+        connect(socket, &QTcpSocket::readyRead, this, &Widget::ReadData);
     }
-    else{
+    else
+    {
         ui->textEdit->append("The connection failed");
     }
 
     // //美化
     // ui->pushButton->setStyleSheet("QPushButton{color : #ffaa00; }");
-    ui->label_3->setStyleSheet("font: 25 10pt '微软雅黑 Light';color: white;" "background-color:rgba(255,255,255,0)");
+    ui->label_3->setStyleSheet("font: 25 10pt '微软雅黑 Light';color: white;"
+                               "background-color:rgba(255,255,255,0)");
 
     // ui->textEdit->setStyleSheet("font: '微软雅黑 Light';font-color: white;");
 }
@@ -46,7 +47,8 @@ Widget::~Widget()
     db.close(); // 关闭数据库连接
 }
 
-void Widget::ReadData(){
+void Widget::ReadData()
+{
     QByteArray buf = socket->readAll();
     ui->textEdit->append(buf);
     saveChatData(QString(buf)); // 存储聊天记录到数据库
@@ -56,11 +58,11 @@ void Widget::on_btn_push_clicked()
 {
     QString target = ui->target_line->text();
     QString tmp = QString("$%1$[用户名:%2]").arg(target).arg(id);
-    QByteArray data =(tmp+ui->lineEdit_3->text()) .toUtf8();
+    QByteArray data = (tmp + ui->lineEdit_3->text()).toUtf8();
     ui->lineEdit_3->clear();
     ui->target_line->clear();
     socket->write(data);
-    saveChatData(tmp); // 存储聊天记录到数据库
+    saveChatData(QString(data)); // 存储聊天记录到数据库
 }
 
 //***************
@@ -68,7 +70,8 @@ void Widget::openDatabase()
 {
     db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("chat_history.db");
-    if (!db.open()) {
+    if (!db.open())
+    {
         qDebug() << "Error: Failed to connect database." << db.lastError();
     }
 }
@@ -77,7 +80,8 @@ void Widget::createTable()
 {
     QSqlQuery query;
     QString create_sql = "create table if not exists chat_history (id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)";
-    if (!query.exec(create_sql)) {
+    if (!query.exec(create_sql))
+    {
         qDebug() << "Error: Failed to create table." << query.lastError();
     }
 }
@@ -86,18 +90,19 @@ void Widget::clearChatHistory()
 {
     QSqlQuery query;
     QString clear_sql = "DELETE FROM chat_history";
-    if (!query.exec(clear_sql)) {
+    if (!query.exec(clear_sql))
+    {
         qDebug() << "Error: Failed to clear chat history." << query.lastError();
     }
 }
-
 
 void Widget::saveChatData(const QString &message)
 {
     QSqlQuery query;
     query.prepare("INSERT INTO chat_history (message) VALUES (:message)");
     query.bindValue(":message", message);
-    if (!query.exec()) {
+    if (!query.exec())
+    {
         qDebug() << "Error: Failed to insert data." << query.lastError();
     }
 }
@@ -108,6 +113,4 @@ void Widget::on_search1_clicked()
     // this->close();
     searchwidget *sch = new searchwidget();
     sch->show();
-
 }
-
